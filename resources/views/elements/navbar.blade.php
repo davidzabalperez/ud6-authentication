68a23fb7d9e2066adfa37fad796f3a711c8315b4
davidzabalperez <davidzabalperez@gmail.com>
2018-12-03 12:27:47 +0100

    navbar /

--- resources/views/elements/navbar.blade.php
+++ resources/views/elements/navbar.blade.php
@@ -9,38 +9,54 @@
 
         <div class="collapse navbar-collapse" id="navbar">
 
-            <ul class="navbar-nav mr-auto">
-                <li id="initial" class="nav-item">
-                    <a class="nav-link" href="#inicio">
-                        Inicio
-                    </a>
-                </li>
-            </ul>
+           <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+               
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <ul class="navbar-nav navbar-right ">
-                    <li class="nav-item dropdown active">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa fa-flag"></i>
-                        Idioma
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Euskara</a>
-                        <a class="dropdown-item active" href="#">Castellano</a>
-                      </div>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/getLogin" >
-                            <i class="fa fa-sign-in"></i>
-                            Login
-                        </a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/getRegister">
-                            <i class="fa fa-user-plus"></i>
-                            Registro
-                        </a>
-                    </li>
-            </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                @if (Route::has('register'))
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
+                            </li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
         </div>
     </div>
 </nav>
